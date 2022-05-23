31c5d2b47c28bc7d92e4059aa8504d4ef94fe777
Isiah Parra <isiahparra@Isiahs-MacBook-Pro.local>
2022-05-23 15:25:24 -0600

    Finished Moonshot

--- CoinBrowser/CoinBrowser/Controller/CoinController.swift
+++ CoinBrowser/CoinBrowser/Controller/CoinController.swift
@@ -23,10 +23,35 @@ class CoinController {
         // piece together our URL
         // decode the data into our objects
         // once we have our objects we can add them to our SOT
-        guard let baseURL = URL(string: baseURLString) else {return completion(false)}
+        guard let baseURL = URL(string: baseURLString) else {completion(false); return}
         let coinsURL = baseURL.appendingPathComponent(keyCoinsComponent)
         let finalURL = coinsURL.appendingPathComponent(keyListComponent)
         print(finalURL)
+        URLSession.shared.dataTask(with: finalURL) { coinData, _ , error in
+            if let error = error {
+                print("There was an \(error.localizedDescription)")
+                completion(false)
+            }
+            guard let data = coinData else {completion(false); return}
+            do{
+                if let topLevelArrayOfCoinDictionaries = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [[String:String]] {
+                    for coinDictionary in topLevelArrayOfCoinDictionaries {
+                        if let id = coinDictionary["id"],
+                           let symbol = coinDictionary["symbol"],
+                           let name = coinDictionary["name"] {
+                            let parsedCoin = Coin(id: id, symbol: symbol, name: name)
+                            coins.append(parsedCoin)
+                        }
+                    }
+                }
+                completion(true)
+            }catch {
+                print("Error in do/try/catch \(error.localizedDescription)")
+                completion(false)
+                
+            }
+
+        }.resume()
     }
     
 }// End of class
