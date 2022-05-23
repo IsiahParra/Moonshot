a5c0069e56bf0b8e37cac615bd2049a85d36e68a
Isiah Parra <isiahparra@Isiahs-MacBook-Pro.local>
2022-05-23 13:37:50 -0600

    Updated Model

---
+++ CoinBrowser/CoinBrowser/Model/Coin.swift
@@ -0,0 +1,21 @@
+//
+//  Coin.swift
+//  CoinBrowser
+//
+//  Created by Isiah Parra on 5/23/22.
+//
+
+import Foundation
+
+class Coin {
+    let id: String
+    let symbol: String
+    let name: String
+    
+    init( id: String, symbol: String, name: String) {
+        self.id = id
+        self.symbol = symbol
+        self.name = name
+    }
+    
+}// End of class
